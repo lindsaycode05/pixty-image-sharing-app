34a48524c0d51959a145da44ff725d9ec4800abf
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-16 20:48:38 +0200

    Add user google id to display some data in the app

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,16 +1,49 @@
 import React, { useState, useRef, useEffect } from 'react';
 import '../../helpers/_utils.scss';
 import './home.scss';
+import logo from '../../assets/logo.png';
+import { Link, Routes, Route } from 'react-router-dom';
+import { client } from '../../client';
+
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { Link, Routes, Route } from 'react-router-dom';
 import { UserProfile, Sidebar } from '../../components';
-import { client } from '../../client';
-import logo from '../../assets/logo.png';
 import Pins from '../pins/Pins';
+import { userQuery } from '../../utils/data';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const userInfo =
+    localStorage.getItem('user') !== undefined
+      ? JSON.parse(localStorage.getItem('user'))
+      : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+
+  return (
+    <div className='home'>
+      <div className='home__desktop'>
+        <Sidebar />
+      </div>
+      <div className='home__mobile flex'>
+        <HiMenu fontSize={40} onClick={() => setToggleSidebar(true)} />
+        <Link to='/'>
+          <img src={logo} alt='logo' />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          {/* <img src={logo} alt="logo" /> */}
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
