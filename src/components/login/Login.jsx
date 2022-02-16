bc58f7f855abb45ea6b51e300f630cd9b9d95e61
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-16 15:45:18 +0200

    Add markup for 'Login' page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import '../../helpers/_utils.scss';
-import './login.scss'
+import './login.scss';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../../assets/share.mp4';
-import logo from '../../assets/logo-white.png';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
-  return <div className='login'>
-
-  </div>;
+  return (
+    <div className='login'>
+      <video
+        src={shareVideo}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+        autoPlay
+      ></video>
+      <div className='login__form flex-col'>
+        <img src={logo} alt='logo' />
+        <GoogleLogin 
+        clientId=''
+        render={(renderProps) => (
+         <button
+         className='flex' >
+            <FcGoogle/> Sign in with Google
+         </button>
+        )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
