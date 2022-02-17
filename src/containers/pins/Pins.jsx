4dd0e961c47ae6d153a4ace78af6b44647718945
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-17 18:03:19 +0200

    Create routes for navigation

--- src/containers/pins/Pins.jsx
+++ src/containers/pins/Pins.jsx
@@ -1,10 +1,26 @@
 import './pins.scss';
-import '../../helpers/_utils.scss'; 
+import '../../helpers/_utils.scss';
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../../components';
 
-const Pins = () => {
-  return <div>Pins</div>;
+const Pins = ({user}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return <div className='pins'>
+    <div className="pins__navbar">
+      <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    </div>
+    <div className="pins__routes">
+      <Routes>
+        <Route path='/' element={<Feed/>}/>
+        <Route path='/category/:categoryId' element={<Feed/>}/>
+        <Route path='/pin-detail/:pinId' element={<PinDetail user={user}/>}/>
+        <Route path='/create-pin' element={<CreatePin user={user}/>}/>
+        <Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>}/>
+      </Routes>
+    </div>
+  </div>;
 };
 
 export default Pins;
